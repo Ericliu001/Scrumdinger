312d0eca4f08326be35ef898f759842ea5e642e3
Eric Liu <eric.liu.developer@gmail.com>
2024-06-10 11:25:23 +1000

    init

---
+++ Scrumdinger/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Eric Liu on 6/10/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+
+    var body: some View {
+        
+        VStack {
+            ProgressView(value: 5, total: 15)
+            
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Placeholder")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            
+            Circle()
+                .strokeBorder(lineWidth: 24)
+
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
+    }
+
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(timestamp: Date())
+            modelContext.insert(newItem)
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
