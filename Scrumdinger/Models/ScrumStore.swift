5c147ac73c81e1d7d57a17d08f5827b611d3ee09
Eric Liu <eric.liu.developer@gmail.com>
2024-06-20 14:47:00 +1000

    saves data on disk

---
+++ Scrumdinger/Models/ScrumStore.swift
@@ -0,0 +1,45 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by Eric Liu on 6/20/24.
+//
+
+import SwiftUI
+
+@Observable
+class ScrumStore {
+    var scrums: [DailyScrum] = [ ]
+    
+    
+    private static func fileURL( ) throws -> URL {
+        try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("scrums.data")
+    }
+    
+    
+    func load( ) async throws {
+        let task = Task<[DailyScrum], Error> {
+            let fileURL = try Self.fileURL()
+            guard let data = try? Data(contentsOf: fileURL) else {
+                return [ ]
+            }
+            
+            let dailyScrum = try JSONDecoder().decode([DailyScrum].self, from: data)
+            return dailyScrum
+        }
+        
+        let scrums = try await task.value
+        self.scrums = scrums
+    }
+    
+    func save(scrums: [DailyScrum]) async throws {
+        let task = Task {
+            let data = try JSONEncoder().encode(scrums)
+            let outfile = try Self.fileURL()
+            try data.write(to: outfile)
+        }
+        
+        _ = try await task.value
+    }
+}
--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -12,6 +12,8 @@ import SwiftData
 struct ScrumdingerApp: App {
     @State private var scrums = DailyScrum.sampleData
     
+    @State private var store: ScrumStore = ScrumStore()
+    
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
@@ -27,7 +29,22 @@ struct ScrumdingerApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: $scrums)
+            ScrumsView(scrums: $store.scrums) {
+                Task {
+                    do {
+                        try await store.save(scrums: store.scrums)
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
+            }
+                .task {
+                    do {
+                        try await store.load()
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
         }
         .modelContainer(sharedModelContainer)
     }
